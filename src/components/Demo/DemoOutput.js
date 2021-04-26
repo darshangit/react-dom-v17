5f892e4d28d8f4025686b8362ff168069abe289f
Darshan Satya <darshans1989@gmail.com>
2021-04-26 12:49:19 -0400

    react memo + optimization

---
+++ src/components/Demo/DemoOutput.js
@@ -0,0 +1,9 @@
+import { memo } from "react";
+import MyParagraph from "./MyParagraph";
+
+const DemoOutput = (props) => {
+  console.log("DemoOutput");
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
+};
+
+export default memo(DemoOutput); // will be executed only if the props change
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,6 @@
+const MyParagraph = (props) => {
+  console.log("paraoOutput");
+  return <p>{props.children}</p>;
+};
+
+export default MyParagraph;
